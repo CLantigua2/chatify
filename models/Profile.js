135ffff5b4ceebb18f991f75fe65ed55f06e7e99
Carlos Lantigua <clantigua954@gmail.com>
2018-12-25 06:25:53 -0500

    completed server models

---
+++ models/Profile.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProfileSchema = new Schema({
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: 'users'
+	},
+	handle: {
+		type: String,
+		required: true,
+		max: 40
+	},
+	theme: {
+		sidebar: {
+			type: String
+		},
+		active: {
+			type: String
+		},
+		inactive: {
+			type: String
+		},
+		header: {
+			type: String
+		}
+	},
+	date: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
