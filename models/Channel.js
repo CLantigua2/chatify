4790368faf7b3732dea785c030ffdbd02ba7f4c9
Carlos Lantigua <clantigua954@gmail.com>
2018-12-26 22:52:34 -0500

    created channel model

--- models/Channel.js
+++ models/Channel.js
@@ -1,1 +1,22 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ChannelSchema = new Schema({
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: 'users'
+	},
+	name: {
+		type: String,
+		required: true
+	},
+	purpose: {
+		type: String
+	},
+	date: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+module.exports = Channel = mongoose.model('channel', ChannelSchema);
