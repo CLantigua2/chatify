6678de33e4c5108813f162adfc81b24ba819308c
Carlos Lantigua <clantigua954@gmail.com>
2018-12-28 22:26:29 -0500

    created drop down menu and styled it. It looks fire emojie

--- client/src/components/chatify/dashboard/sidebar/heading/Heading.js
+++ client/src/components/chatify/dashboard/sidebar/heading/Heading.js
@@ -3,15 +3,60 @@ import { connect } from 'react-redux';
 import styled from 'styled-components';
 
 class Heading extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			showMenu: false
+		};
+	}
+
+	showMenu = (e) => {
+		e.preventDefault();
+		this.setState({
+			showMenu: !this.state.showMenu
+		});
+	};
+
+	// closeMenu = (e) => {
+	// 	if (this.dropdownMenu.contains(e.target)) {
+	// 		this.setState({ showMenu: false }, () => {
+	// 			document.removeEventListener('click', this.closeMenu);
+	// 		});
+	// 	}
+	// };
 	render() {
 		const { auth } = this.props;
 		console.log(auth);
 		return (
 			<div>
-				<Container>
-					<Image src={auth.user.avatar} alt={auth.user.name} />
-					<H4>{auth.user.name}</H4>
-				</Container>
+				<Wrapper>
+					<Container>
+						<Image src={auth.user.avatar} alt={auth.user.name} />
+						<H4>{auth.user.name}</H4>
+					</Container>
+					<Dontmove>
+						<i onClick={this.showMenu} className="fas fa-cog" />
+						{this.state.showMenu ? (
+							<ul>
+								<li>
+									<i className="fas fa-user-cog" />
+									Edit Profile
+								</li>
+								<hr />
+								<li>
+									<i className="far fa-list-alt" />
+									See Channels
+								</li>
+								<hr />
+								<li>
+									<i className="far fa-trash-alt" />
+									Delete Account
+								</li>
+								<hr />
+							</ul>
+						) : null}
+					</Dontmove>
+				</Wrapper>
 				<MakeChannel>
 					<p>Make a channel</p>
 					<button>
@@ -28,6 +73,61 @@ const mapStateToProps = (state) => ({
 });
 
 export default connect(mapStateToProps)(Heading);
+
+const Dontmove = styled.div`
+	position: absolute;
+	left: 195px;
+`;
+
+const Wrapper = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	/* align-items: center; */
+	i {
+		font-size: 2rem;
+		color: ${(props) => props.theme.inactive};
+		cursor: pointer;
+		transition: 1s ease-in-out;
+		position: relative;
+		left: 5px;
+		&:hover {
+			color: ${(props) => props.theme.active};
+			transform: rotate(180deg);
+		}
+	}
+	ul {
+		margin-top: 20px;
+		box-shadow: 0px 0px 11px -1px rgba(0, 0, 0, 0.75);
+		background: #ffffff;
+		width: 100%;
+		height: 224px;
+		padding: 10px;
+		border: 1px solid slategray;
+		border-radius: 4px;
+		hr {
+			border-color: rgba(0, 0, 0, 0.1);
+		}
+		li:first-child {
+			margin-top: 20px;
+		}
+
+		li {
+			color: ${(props) => props.theme.inactive};
+			cursor: pointer;
+			transition: 0.4s ease-in-out;
+			margin-bottom: 22px;
+			margin-top: 22px;
+			&:hover {
+				color: ${(props) => props.theme.active};
+			}
+
+			i {
+				margin-right: 20px;
+			}
+		}
+	}
+`;
 
 const Container = styled.div`
 	display: flex;
