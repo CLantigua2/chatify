31e9a2bebbf7afc2af1b12a02378895eb2e60c89
Carlos Lantigua <clantigua954@gmail.com>
2018-12-31 18:03:34 -0500

    fixed base test for server

--- server.spec.js
+++ server.spec.js
@@ -1,27 +1,13 @@
 const request = require('supertest');
-const express = require('express');
-const server = express();
+const server = require('./server');
 
 describe('index.js', () => {
-	it('should return 200 status', () => {
-		request(server).get('/').expect(200).end((err, res) => (err ? res.json(err) : null));
-	});
-	describe('user routes', () => {
-		it('should return 200 for user route', () => {
-			request(server).get('/api/users').expect(200).end((err, res) => (err ? res.json(err) : null));
-		});
-		it('should register a user and return 201 status', () => {
-			request(server)
-				.post('/api/users/register')
-				.send({
-					name: 'Steve',
-					email: 'Steve@hotmail.com',
-					password: 'getMoneySteve',
-					password2: 'getMoneySteve'
-				})
-				.set('Autherized', 'application/json')
-				.expect(201)
-				.end((err, res) => (err ? res.json(err) : null));
+	describe('/ route', () => {
+		it('should return status code 200', () => {
+			// hit the endpoint and get the response
+			request(server).get('/').then((response) => {
+				expect(response.status).toBe(200);
+			});
 		});
 	});
 });
