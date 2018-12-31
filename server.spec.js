336843b124c805843cad64776ac987de0b099a60
Carlos Lantigua <clantigua954@gmail.com>
2018-12-31 18:12:47 -0500

    testing data type to be json

--- server.spec.js
+++ server.spec.js
@@ -3,11 +3,13 @@ const server = require('./server');
 
 describe('index.js', () => {
 	describe('/ route', () => {
-		it('should return status code 200', () => {
-			// hit the endpoint and get the response
-			request(server).get('/').then((response) => {
-				expect(response.status).toBe(200);
-			});
+		it('should return status code 200', async () => {
+			let response = await request(server).get('/');
+			expect(response.status).toBe(200);
+		});
+		it('Should return JSON', async () => {
+			let response = await request(server).get('/');
+			expect(response.type).toBe('application/json');
 		});
 	});
 });
