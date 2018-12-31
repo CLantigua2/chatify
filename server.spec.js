b86a39a9c5f4005251c26992cdc93a252e38b7c7
Carlos Lantigua <clantigua954@gmail.com>
2018-12-31 16:25:35 -0500

    created end function to be reused in tests

--- server.spec.js
+++ server.spec.js
@@ -2,10 +2,15 @@ const request = require('supertest');
 const express = require('express');
 const server = express();
 
+end = (err, res) => {
+	if (err) res.json(err);
+};
+
 describe('index.js', () => {
 	it('should return 200 status', () => {
-		request(server).get('/').expect(200).end((err, res) => {
-			if (err) res.json(err);
-		});
+		request(server).get('/').expect(200).end();
+	});
+	it('should return 200 for user route', () => {
+		request(server).get('/api/users').expect(200).end();
 	});
 });
