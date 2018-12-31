6c112604fbcfdbf4449cfaa74c24107478de960e
Carlos Lantigua <clantigua954@gmail.com>
2018-12-31 16:29:43 -0500

    tested user register route

--- server.spec.js
+++ server.spec.js
@@ -13,4 +13,12 @@ describe('index.js', () => {
 	it('should return 200 for user route', () => {
 		request(server).get('/api/users').expect(200).end();
 	});
+	it('should register a user', () => {
+		request(server)
+			.post('/api/users/register')
+			.send({ name: 'Steve', email: 'Steve@hotmail.com', password: 'getMoneySteve', password2: 'getMoneySteve' })
+			.set('Autherized', 'application/json')
+			.expect(200)
+			.end();
+	});
 });
