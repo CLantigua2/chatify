4800e83282b0ee14723283c2e86de6da88a821c5
Carlos Lantigua <clantigua954@gmail.com>
2019-01-01 09:09:15 -0500

    tested for auth token requirement

--- server.spec.js
+++ server.spec.js
@@ -5,6 +5,23 @@ const users = '/api/users/';
 const profile = '/api/profile/';
 const channels = '/api/channels/';
 
+let token;
+
+beforeAll((done) => {
+	request(server)
+		.post(`${users}/login`)
+		.send({
+			// name: 'username',
+			email: 'user@hotmail.com',
+			// password: 'password',
+			password2: 'password'
+		})
+		.end((err, response) => {
+			token = response.body.token;
+			done();
+		});
+});
+
 describe('server.js', () => {
 	describe('/ route', () => {
 		it('should return status code 200', async () => {
@@ -22,6 +39,10 @@ describe('/api/users route', () => {
 	it('should return status 200 for user route', async () => {
 		let response = await request(server).get(`${users}/test`);
 		expect(response.status).toBe(200);
+	});
+	it('should require auth token and give 401 code', async () => {
+		let response = await request(server).get(`${users}/current`);
+		expect(response.status).toBe(401);
 	});
 });
 describe('/api/profile route', () => {
