5e9b0a0815db93979044ad6051be64d9a3d2e9a6
Carlos Lantigua <clantigua954@gmail.com>
2019-01-01 12:05:44 -0500

    tested client user routes

--- server.spec.js
+++ server.spec.js
@@ -1,26 +1,15 @@
 const request = require('supertest');
 const server = require('./server');
-
+const User = require('./models/User');
+const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+mongoose
+	.connect(db, { useNewUrlParser: true })
+	.then(() => console.log('MongoDB Connected'))
+	.catch((err) => console.log(err));
 const users = '/api/users/';
 const profile = '/api/profile/';
 const channels = '/api/channels/';
-
-let token;
-
-beforeAll((done) => {
-	request(server)
-		.post(`${users}/login`)
-		.send({
-			// name: 'username',
-			email: 'user@hotmail.com',
-			// password: 'password',
-			password2: 'password'
-		})
-		.end((err, response) => {
-			token = response.body.token;
-			done();
-		});
-});
 
 describe('server.js', () => {
 	describe('/ route', () => {
@@ -36,6 +25,19 @@ describe('server.js', () => {
 });
 
 describe('/api/users route', () => {
+	beforeAll(async () => {
+		await User.remove({});
+	});
+
+	// afterEach(async () => {
+	// 	await User.remove({});
+	// });
+	afterAll(async () => {
+		await mongoose.connection.close();
+	});
+	it('has a module', () => {
+		expect(User).toBeDefined();
+	});
 	it('should return status 200 for user route', async () => {
 		let response = await request(server).get(`${users}/test`);
 		expect(response.status).toBe(200);
@@ -43,6 +45,25 @@ describe('/api/users route', () => {
 	it('should require auth token and give 401 code', async () => {
 		let response = await request(server).get(`${users}/current`);
 		expect(response.status).toBe(401);
+	});
+	it('return 404 if logging in a nonexistent user', async () => {
+		const agent = await request(server);
+		const response = await agent.post(`${users}/login`).send({
+			email: 'Charles@gmail.com',
+			password: 'MrCharles'
+		});
+		expect(response.status).toEqual(404);
+	});
+
+	it('return 200 and type JSON if registering a user', async () => {
+		const agent = await request(server);
+		const response = await agent.post(`${users}/register`).send({
+			name: 'Charles',
+			email: 'Charles@email.com',
+			password: 'MrCharles',
+			password2: 'MrCharles'
+		});
+		expect(response.status).toEqual(200);
 	});
 });
 describe('/api/profile route', () => {
