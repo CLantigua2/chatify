df78f1fd9226c1aeae191cd82a060e19d72ae042
Carlos Lantigua <clantigua954@gmail.com>
2019-01-05 15:51:03 -0500

    created a state validator function server side

--- client/src/components/profile/ProfileHeader.js
+++ client/src/components/profile/ProfileHeader.js
@@ -2,14 +2,44 @@ import React, { Component } from 'react';
 import isEmpty from '../../validation/is-empty';
 import styled from 'styled-components';
 import TextFieldGroup from '../common/TextFieldGroup';
+import { createProfile, getCurrentProfile } from '../../redux/actions/profileActions';
+import { connect } from 'react-redux';
 
 class ProfileHeader extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			isEditing: false,
-			newStatus: ''
+			username: '',
+			status: '',
+			city: '',
+			state: '',
+			errors: {}
 		};
+	}
+
+	// componentDidMount() {
+	// 	this.props.getCurrentProfile();
+	// }
+
+	componentDidUpdate(prevProps) {
+		if (prevProps.errors !== this.props.errors) {
+			this.setState({ errors: this.props.errors });
+		}
+
+		if (this.props.profile.profile) {
+			const profile = this.props.profile.profile;
+
+			const { username = '', status = '', location: { city = '', state = '' } } = profile;
+
+			if (prevProps.profile !== this.props.profile) {
+				this.setState({ username, status, city, state });
+			}
+		}
+
+		if (isEmpty(this.props.profile.profile)) {
+			this.props.getCurrentProfile();
+		}
 	}
 
 	changeHandler = (e) => {
@@ -17,38 +47,114 @@ class ProfileHeader extends Component {
 	};
 
 	setEdit = (e) => {
-		this.setState({ isEditing: !this.state.isEditing });
+		this.setState({ isEditing: true, oldStatus: this.props.profile.status });
+	};
+
+	cancelEdit = (e) => {
+		this.setState({ isEditing: false, newStatus: this.props.profile.status });
+	};
+
+	submitProfile = (e) => {
+		const profileData = {
+			username: this.state.username,
+			status: this.state.status,
+			location: this.state.location
+		};
+		this.props.createProfile(profileData);
 	};
 
 	render() {
-		const { profile } = this.props;
-		const { isEditing } = this.state;
+		const { profile } = this.props.profile;
+		const { isEditing, errors, username, status, city, state } = this.state;
+		let editInputs;
+		if (isEditing === true) {
+			editInputs = (
+				<div>
+					<TextFieldGroup
+						type="text"
+						placeholder="New username.."
+						value={username}
+						name="username"
+						handleChange={this.changeHandler}
+						errors={errors.username}
+					/>
+					<TextFieldGroup
+						placeholder="New status..."
+						type="text"
+						value={status}
+						name="status"
+						handleChange={this.changeHandler}
+						errors={errors.status}
+					/>
+					<TextFieldGroup
+						placeholder="City..."
+						type="text"
+						value={city}
+						name="city"
+						handleChange={this.changeHandler}
+						errors={errors.city}
+					/>
+					<TextFieldGroup
+						placeholder="State..."
+						type="text"
+						value={state}
+						name="state"
+						handleChange={this.changeHandler}
+						errors={errors.state}
+					/>
+				</div>
+			);
+		} else {
+			editInputs = (
+				<div>
+					{isEmpty(username) ? null : <p className="line">{username}</p>}
+					{isEmpty(status) ? (
+						<p className="line">
+							Y U No Make Status{' '}
+							<span role="img" aria-label="Why">
+								🤷‍
+							</span>?
+						</p>
+					) : (
+						<p className="line">{profile.status}</p>
+					)}
+					{isEmpty(city) ? null : <p className="line">City: {city}</p>}
+					{isEmpty(state) ? null : <p className="line">State: {state}</p>}
+				</div>
+			);
+		}
 		return (
 			<Container>
 				<img className="rounded-circle" src={profile.user.avatar} alt="user avatar" />
 				<div className="text-center">
 					<h1 className="user-name">{profile.user.name}</h1>
-					{isEmpty(profile.username) ? null : <p className="line">{profile.username}</p>}
-					{isEmpty(profile.status) ? null : <p className="line">{profile.status}</p>}
+					{editInputs}
 					{isEditing ? (
 						<div>
-							<button onClick={() => this.setEdit()} className="accept-btn">
+							<button className="accept-btn" onClick={() => this.submitProfile()}>
 								Accept
 							</button>
-							<button className="cancel-btn">Cancel</button>
+							<button className="cancel-btn" onClick={() => this.cancelEdit()}>
+								Cancel
+							</button>
 						</div>
 					) : (
-						<button className="edit-btn">Edit Status</button>
+						<button className="edit-btn" onClick={() => this.setEdit()}>
+							Edit Status
+						</button>
 					)}
-					{isEmpty(profile.city) ? null : <p className="line">City: {profile.city}</p>}
-					{isEmpty(profile.state) ? null : <p className="line">State: {profile.state}</p>}
 				</div>
 			</Container>
 		);
 	}
 }
 
-export default ProfileHeader;
+const mapStateToProps = (state) => ({
+	profile: state.profile,
+	errors: state.errors
+});
+
+export default connect(mapStateToProps, { createProfile, getCurrentProfile })(ProfileHeader);
 
 const Container = styled.div`
 	background-color: rgba(71, 125, 219, 1);
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,4 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import invariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import rootReducer from './reducers';
@@ -7,11 +9,17 @@ import throttle from 'lodash/throttle';
 
 // calls to load state from local storage
 // create an initial state object
-const middleware = [ thunk, createLogger() ];
+const middleware = [ invariant(), thunk, createLogger() ];
 const persistedState = loadFromLocalStore();
-
-const store = createStore(rootReducer, persistedState, compose(applyMiddleware(...middleware)));
+const composeEnhancers = composeWithDevTools({ trace: true, traceLimit: 25 });
+const store = createStore(rootReducer, persistedState, composeEnhancers(applyMiddleware(...middleware)));
 
 store.subscribe(throttle(() => saveToLocalStorage(store.getState())), 1000);
+
+if (module.hot) {
+	module.hot.accept('./reducers/', () => {
+		store.replaceReducer(require('./reducers/').default);
+	});
+}
 
 export default store;
