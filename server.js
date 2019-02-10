6eaae021ed6573f7f74ecfd9c09510a532eb6312
Carlos Lantigua <clantigua954@gmail.com>
2019-02-10 17:18:25 -0500

    didn't changed much, out of options

--- server.js
+++ server.js
@@ -64,7 +64,5 @@ if (process.env.NODE_ENV === "production") {
   })
 }
 
-const port = process.env.PORT || 9000
-server.listen(port, () => {
-  console.log(`This server is over ${port}`)
-})
+// const port = process.env.PORT || 9000
+server.listen(process.env.PORT || 9000)
